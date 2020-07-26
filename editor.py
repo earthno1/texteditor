646752349909e197c950433b020b21f5271113c5
cxzlw <wsc_zds@qq.com>
2020-07-26 22:38:58 +0800

    开始合并（gl的代码太简短，故将其代码合并到我这里了哈）

--- editor.py
+++ editor.py
@@ -1,1 +1,133 @@
-# 还没开发
+'''pyinstaller Editor.py --add-data="./源码.ico;." -F -w -i 源码.ico'''
+import os, sys
+from tkinter import *
+from tkinter.filedialog import asksaveasfilename, askopenfilename
+from tkinter.messagebox import showinfo
+
+main = Tk()
+path = None
+main.title("创新者.编辑器 - 新的文本文件")
+main.geometry("500x500")
+zhidinv = False
+
+
+def resource_path(relative_path):
+    '''返回资源绝对路径。'''
+    if hasattr(sys, '_MEIPASS'):
+        # PyInstaller会创建临时文件夹temp
+        # 并把路径存储在_MEIPASS中
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath('.')
+    return os.path.join(base_path, relative_path)
+
+
+main.iconbitmap(resource_path(".\\源码.ico"))
+
+
+def zhidin():
+    global zhidinv
+    if zhidinv:
+        main.wm_attributes('-topmost', 0)
+        zhidinv = False
+    else:
+        main.wm_attributes('-topmost', 1)
+        zhidinv = True
+
+
+def open_file():
+    global path
+    path = askopenfilename(title="创新者.编辑器 - 打开", filetypes=[("文本文件", "*.txt"), ("所有文件", "*.*")])
+    if path and path != "":
+        main.title("创新者.编辑器 - " + path)
+        editor.delete("1.0", END)
+        with open(path, "r", encoding="utf-8") as f:
+            editor.insert(END, f.read())
+
+
+def save_as_file():
+    global path
+    path = asksaveasfilename(title="创新者.编辑器 - 另存为", filetypes=[("文本文件", "*.txt"), ("所有文件", "*.*")])
+    if path and path != "":
+        main.title("创新者.编辑器 - " + path)
+        with open(path, "w", encoding="utf-8") as f:
+            f.write(editor.get("1.0", END))
+
+
+def save_file():
+    global path
+    if path and path != "":
+        with open(path, "w", encoding="utf-8") as f:
+            f.write(editor.get("1.0", END))
+    elif path is None:
+        path = asksaveasfilename(title="创新者.编辑器 - 保存", filetypes=[("文本文件", "*.txt"), ("所有文件", "*.*")])
+        if path and path != "":
+            main.title("创新者.编辑器 - " + path)
+            with open(path, "w", encoding="utf-8") as f:
+                f.write(editor.get("1.0", END))
+
+
+def new_file():
+    global path
+    path = None
+    main.title("创新者.编辑器 - 新的文本文件")
+    editor.delete("1.0", END)
+
+
+def info():
+    showinfo('关于', '此程序由创新者.老王亲手打造，只为装逼2333')
+
+
+# op = Frame()
+def quitexit():
+    sys.exit()
+    quit()
+
+
+menu = Menu(main)
+file = Menu(menu, tearoff=0)
+file.add_command(label="新建", command=new_file)
+file.add_command(label="打开", command=open_file)
+file.add_command(label="保存", command=save_file)
+file.add_command(label="另存为", command=save_as_file)
+
+menu.add_cascade(label="文件", menu=file)
+menu.add_cascade(label="编辑")
+menu.add_cascade(label="格式")
+menu.add_cascade(label="查看")
+
+mhelp = Menu(menu, tearoff=0)
+mhelp.add_command(label="关于", command=info)
+menu.add_cascade(label="帮助", menu=mhelp)
+mcxz = Menu(menu, tearoff=0)
+# zhidinbtn = mcxz.add_command(label = "置顶 x", command = zhidin)
+mcxz.add_checkbutton(label="保持置顶", command=zhidin)
+menu.add_cascade(label="特殊功能", menu=mcxz)
+menu.add_command(label="退出", command=quitexit)
+
+
+def bigwmain():
+    main.update()
+    editor["width"] = main.winfo_width()
+    main.update()
+    editor["height"] = main.winfo_height()
+    main.update()
+
+
+# newf = Button(master=op,text = "新建",command = new_file)
+# openf = Button(master=op,text = "打开文件",command = open_file)
+# saveasf = Button(master=op,text = "另存为",command = save_as_file)
+# savef = Button(master=op,text = "保存",command = save_file)
+# newf.pack()
+# openf.pack()
+# saveasf.pack()
+# savef.pack()
+editor = Text()
+# op.pack(side = LEFT)
+editor.pack(side=LEFT, expand=1)
+# editor.bind('<KeyPress>',func=bigwmain)
+main.config(menu=menu)
+# timer = threading.Timer(1, fresher)
+# timer.start()
+bigwmain()
+main.mainloop()
